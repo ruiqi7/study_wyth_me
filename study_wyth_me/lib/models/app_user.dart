9ef837d681107b3ea3b852941e4a7200b7c57b44
szeying02 <97298202+szeying02@users.noreply.github.com>
2022-05-22 22:35:24 +0800

    Read User Data from Firestore
    - username read from Firestore integrated into the Home appBar
    - abstracted the appBar with username read from Firestore, for non-Home pages

--- study_wyth_me/lib/models/app_user.dart
+++ study_wyth_me/lib/models/app_user.dart
@@ -1,5 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:study_wyth_me/main.dart';
+
 class AppUser {
+
+  final String uid;
   final String username;
 
-  AppUser({ required this.username });
+  AppUser({ required this.uid, required this.username });
+
+//obtain user data from Firestore as AppUser Object
+//factory AppUser.fromFirestore(
+//    DocumentSnapshot<Map<String, dynamic>> snapshot,
+//    SnapshotOptions? options,
+//    ) {
+//  final data = snapshot.data();
+//  return AppUser(
+//    username: data?['username'],
+//  );
+//}
+
+//Map<String, dynamic> toFirestore() {
+//  return {
+//    "username": username,
+//  };
+//}
+
 }
--- study_wyth_me/lib/shared/bar_widgets.dart
+++ study_wyth_me/lib/shared/bar_widgets.dart
@@ -1,153 +1,205 @@
 import 'package:flutter/material.dart';
 import 'package:study_wyth_me/shared/constants.dart';
 
+import '../models/app_user.dart';
+import '../pages/loading.dart';
+import '../services/database.dart';
+
 const oswaldTextStyle = TextStyle(
   fontFamily: 'Oswald',
   fontSize: 12.5,
   color: Colors.white
 );
 
-bottomBar(context, position) => Row(
-  children: <Widget> [
-    Expanded(
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          color: whiteOpacity20,
-          height: 75.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget> [
-              Container(
-                width: 65,
-                height: 60,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: position == 1 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
-                ),
-                child: TextButton(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Icon(
-                        Icons.home,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Home',
-                        style: oswaldTextStyle,
-                      )
-                    ],
+navigationBar(context, position) => SafeArea(
+  child: Row(
+    children: <Widget> [
+      Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            color: whiteOpacity20,
+            height: 75.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget> [
+                Container(
+                  width: 65,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: position == 1 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
                   ),
-                  onPressed: () {
-                    if (position != 1) {
-                      Navigator.pushNamed(context, '/home');
-                    }
-                  },
-                ),
-              ),
-              Container(
-                width: 65,
-                height: 60,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: position == 2 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
-                ),
-                child: TextButton(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Icon(
-                        Icons.forum_rounded,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Forum',
-                        style: oswaldTextStyle,
-                      )
-                    ],
+                  child: TextButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget> [
+                        Icon(
+                          Icons.home,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Home',
+                          style: oswaldTextStyle,
+                        )
+                      ],
+                    ),
+                    onPressed: () {
+                      if (position != 1) {
+                        Navigator.pushNamed(context, '/home');
+                      }
+                    },
                   ),
-                  onPressed: () {  },
                 ),
-              ),
-              Container(
-                width: 75,
-                height: 60,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: position == 3 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
-                ),
-                child: TextButton(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Icon(
-                        Icons.timer,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Study Timer',
-                        style: oswaldTextStyle,
-                      )
-                    ],
+                Container(
+                  width: 65,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: position == 2 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
                   ),
-                  onPressed: () {
-                    if (position != 3) {
-                      Navigator.pushNamed(context, '/timer');
-                    }
-                  },
-                ),
-              ),
-              Container(
-                width: 75,
-                height: 60,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: position == 4 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
-                ),
-                child: TextButton(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Icon(
-                        Icons.emoji_events_rounded,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Leaderboard',
-                        style: oswaldTextStyle,
-                      )
-                    ],
+                  child: TextButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget> [
+                        Icon(
+                          Icons.forum_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Forum',
+                          style: oswaldTextStyle,
+                        )
+                      ],
+                    ),
+                    onPressed: () {  },
                   ),
-                  onPressed: () {  },
                 ),
-              ),
-              Container(
-                width: 65,
-                height: 60,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  color: position == 5 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
-                ),
-                child: TextButton(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Icon(
-                        Icons.pets_rounded,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Mythics',
-                        style: oswaldTextStyle,
-                      )
-                    ],
+                Container(
+                  width: 75,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: position == 3 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
                   ),
-                  onPressed: () {  },
+                  child: TextButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget> [
+                        Icon(
+                          Icons.timer,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Study Timer',
+                          style: oswaldTextStyle,
+                        )
+                      ],
+                    ),
+                    onPressed: () {
+                      if (position != 3) {
+                        Navigator.pushNamed(context, '/timer');
+                      }
+                    },
+                  ),
                 ),
-              )
-            ],
-          ),
-        )
+                Container(
+                  width: 75,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: position == 4 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
+                  ),
+                  child: TextButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget> [
+                        Icon(
+                          Icons.emoji_events_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Leaderboard',
+                          style: oswaldTextStyle,
+                        )
+                      ],
+                    ),
+                    onPressed: () {  },
+                  ),
+                ),
+                Container(
+                  width: 65,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: position == 5 ? const Color.fromRGBO(0, 34, 75, 0.5) : const Color.fromRGBO(0, 34, 75, 0.0),
+                  ),
+                  child: TextButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget> [
+                        Icon(
+                          Icons.pets_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Mythics',
+                          style: oswaldTextStyle,
+                        )
+                      ],
+                    ),
+                    onPressed: () {  },
+                  ),
+                )
+              ],
+            ),
+          )
+      ),
+    ]
+  ),
+);
+
+appBar(context, uid) => PreferredSize(
+    preferredSize: const Size.fromHeight(75),
+    child: SafeArea(
+      child: Container(
+        color: whiteOpacity20,
+        height: 75.0,
+        child: StreamBuilder<AppUser>(
+            stream: DatabaseService(uid: uid).userData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                AppUser appUser = snapshot.data!;
+                return Row(
+                  //mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget> [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: CircleAvatar(
+                        radius: 25.0,
+                        backgroundImage: NetworkImage('https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          appUser.username,
+                          style: const TextStyle(
+                              fontSize: 27.5,
+                              color: Colors.white,
+                              letterSpacing: 1.5,
+                              fontFamily: 'Chewy'
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Loading();
+              }
+            }
+        ),
+      ),
     ),
-  ]
 );
